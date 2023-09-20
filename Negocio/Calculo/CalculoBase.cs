595577f72d64128d5a73825fdc62c013c73d29a3
Maycon Batista <maycon.batista@seniornortepr.com.br>
2023-09-20 17:02:32 -0300

    Ajuste

--- Negocio/Calculo/CalculoBase.cs
+++ Negocio/Calculo/CalculoBase.cs
@@ -8,6 +8,7 @@ namespace Negocio.Calculo
         public static decimal Indenizacao(decimal media, int diasBase, int diasEstabilidade)
         {
             decimal vlrIndenizacao = media / diasBase * diasEstabilidade;
+            vlrIndenizacao = Math.Round(vlrIndenizacao, 2);
             return vlrIndenizacao;
         }
         //Calculo 13º Salario
@@ -18,11 +19,11 @@ namespace Negocio.Calculo
 
             if (dias >= 15)
             {
-                return _ = (media / 12) * (meses + 1);
+                return _ = Math.Round((media / 12) * (meses + 1), 2);
             }
             else
             {
-                return _ = (media / 12) * meses;
+                return _ = Math.Round((media / 12) * meses, 2);
             }
         }
         //Calculo Ferias
@@ -33,32 +34,32 @@ namespace Negocio.Calculo
 
             if (dias >= 15)
             {
-                return _ = (media / 12) * (meses + 1);
+                return _ = Math.Round((media / 12) * (meses + 1), 2);
             }
             else
             {
-                return _ = (media / 12) * meses;
+                return _ = Math.Round((media / 12) * meses, 2);
             }
         }
         //Calculo de 1 terço sobre a ferias
         public static decimal TercoFerias(decimal valorFerias)
         {
-            return _ = valorFerias / 3;
+            return _ = Math.Round(valorFerias / 3, 2);
         }
         //Calculo de 8% FGTS
         public static decimal FGTS8(decimal valorEstabilidade, decimal decimoTerceiro)
         {
-            return _ = (valorEstabilidade + decimoTerceiro) * 0.08M;
+            return _ = Math.Round((valorEstabilidade + decimoTerceiro) * 0.08M, 2);
         }
         //Calculo de 40% FGTS
         public static decimal FGTS40(decimal FGTS8)
         {
-            return _ = FGTS8 * 0.40M;
+            return _ = Math.Round(FGTS8 * 0.40M, 2);
         }
         //Soma dos Valores a receber na indenização
         public static decimal Total(decimal indenizacao, decimal decimoTerceiro, decimal Ferias, decimal tercoFerias, decimal fgts8, decimal fgts40, decimal complemento)
         {
-            return _ = indenizacao + decimoTerceiro + Ferias + tercoFerias + fgts8 + fgts40 + complemento;
+            return _ = Math.Round(indenizacao + decimoTerceiro + Ferias + tercoFerias + fgts8 + fgts40 + complemento, 2);
         }
     }
 }
